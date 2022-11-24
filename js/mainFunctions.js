6472abfc958154735e5beb963b2d2fe6bda22760
peter.ahn <47676789+taehyunahn@users.noreply.github.com>
2022-11-24 10:56:08 +0900

    Initial commit

---
+++ js/mainFunctions.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    console.log('ready');
+    $('.navbar_wraper').load('/components/navbar.html');    
+    $('.footer_wraper').load('/components/footer.html');
+    
+});
